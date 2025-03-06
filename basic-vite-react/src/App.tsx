9e3a32b7536477ff323e271861030c75b3ff5cb3
kimwz <kimwz.kr@gmail.com>
2025-03-06 13:15:09 +0900

    update basic template

--- basic-vite-react/src/App.tsx
+++ basic-vite-react/src/App.tsx
@@ -1,13 +1,15 @@
 import { useState } from "react";
 import "./App.css";
-
 function App() {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(1);
 
   return (
     <>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button
+          onClick={() => setCount((count) => count + 1)}
+          className="bg-blue-500 text-white p-2 rounded-md"
+        >
           count is {count}
         </button>
       </div>
