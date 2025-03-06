ea72d5f3da75a4e274ea443dc377b870606bfc7b
kimwz <kimwz.kr@gmail.com>
2025-03-06 13:33:02 +0900

    update tailwindcss, because Containervm does not support the way before

--- basic-vite-react/vite.config.ts
+++ basic-vite-react/vite.config.ts
@@ -1,12 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  base: "./",
-  build: {
-    outDir: "dist",
+  plugins: [react()],
+  optimizeDeps: {
+    exclude: ["lucide-react"],
   },
 });
