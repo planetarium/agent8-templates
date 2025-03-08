3cae575d04dacf79f020423762be182ffb9afa4f
kimwz <kimwz.kr@gmail.com>
2025-03-08 11:05:51 +0900

    add 2D,3D template

---
+++ basic-3d/src/App.tsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import "./App.css";
+function App() {
+  const [count, setCount] = useState(1);
+
+  return (
+    <>
+      <div className="card">
+        <button
+          onClick={() => setCount((count) => count + 1)}
+          className="bg-blue-500 text-white p-2 rounded-md"
+        >
+          count is {count}
+        </button>
+      </div>
+    </>
+  );
+}
+
+export default App;
