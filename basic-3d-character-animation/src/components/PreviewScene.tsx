77ab846cca4c5d05a4b44ec4e6276abcee25581e
Jam Park <minmpop@gmail.com>
2025-03-20 18:14:43 +0900

    fix: basic-3d-character-animation move resource define

--- basic-3d-character-animation/src/components/PreviewScene.tsx
+++ basic-3d-character-animation/src/components/PreviewScene.tsx
@@ -1,18 +1,20 @@
-import React, { useRef, useState, useEffect, useCallback } from "react";
+import React, {
+  useRef,
+  useState,
+  useEffect,
+  useCallback,
+  useMemo,
+} from "react";
 import { Canvas } from "@react-three/fiber";
 import { Character } from "./character/Character";
 import { CharacterResource } from "../types/characterResource";
 import { Environment, OrbitControls } from "@react-three/drei";
 import { CharacterAction } from "../constants/character.constant.ts";
 
-interface PreviewSceneProps {
-  characterResource: CharacterResource;
-}
-
 /**
  * Simple 3D character preview scene
  */
-const PreviewScene: React.FC<PreviewSceneProps> = ({ characterResource }) => {
+const PreviewScene: React.FC = () => {
   const [currentAction, setCurrentAction] = useState<CharacterAction>(
     CharacterAction.IDLE
   );
@@ -46,6 +48,36 @@ const PreviewScene: React.FC<PreviewSceneProps> = ({ characterResource }) => {
         break;
     }
   }, []);
+
+  const characterResource: CharacterResource = useMemo(
+    () => ({
+      name: "Default Character",
+      url: "https://agent8-games.verse8.io/assets/3d/characters/space-marine.glb",
+      animations: {
+        IDLE: "https://agent8-games.verse8.io/assets/3d/animations/mixamorig/idle.glb",
+        WALK: "https://agent8-games.verse8.io/assets/3d/animations/mixamorig/walk.glb",
+        RUN: "https://agent8-games.verse8.io/assets/3d/animations/mixamorig/run.glb",
+        JUMP_UP:
+          "https://agent8-games.verse8.io/assets/3d/animations/mixamorig/jump-up.glb",
+        FALL_IDLE:
+          "https://agent8-games.verse8.io/assets/3d/animations/mixamorig/fall-idle.glb",
+        FALL_DOWN:
+          "https://agent8-games.verse8.io/assets/3d/animations/mixamorig/fall-down.glb",
+        PUNCH:
+          "https://agent8-games.verse8.io/assets/3d/animations/mixamorig/punch.glb",
+        MELEE_ATTACK:
+          "https://agent8-games.verse8.io/assets/3d/animations/mixamorig/melee-attack.glb",
+        AIM: "https://agent8-games.verse8.io/assets/3d/animations/mixamorig/aimming.glb",
+        SHOOT:
+          "https://agent8-games.verse8.io/assets/3d/animations/mixamorig/shoot.glb",
+        AIM_RUN:
+          "https://agent8-games.verse8.io/assets/3d/animations/mixamorig/shoot-run.glb",
+        HIT: "https://agent8-games.verse8.io/assets/3d/animations/mixamorig/hit.glb",
+        DIE: "https://agent8-games.verse8.io/assets/3d/animations/mixamorig/death.glb",
+      },
+    }),
+    []
+  );
 
   return (
     <div
