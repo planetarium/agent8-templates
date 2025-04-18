a3c152b094e2d76195b9ea1f6f92b9f8cc227c36
Jam Park <minmpop@gmail.com>
2025-04-18 16:10:12 +0900

    fix: change floor collider to cuboid

--- basic-3d-quarterview/src/components/r3f/Floor.tsx
+++ basic-3d-quarterview/src/components/r3f/Floor.tsx
@@ -2,15 +2,11 @@ import { RigidBody } from '@react-three/rapier';
 
 export const Floor = () => {
   return (
-    <>
-      <RigidBody type="fixed" colliders="trimesh">
-        <group>
-          <mesh rotation={[-Math.PI / 2, 0, 0]} receiveShadow position={[0, 0, 0]}>
-            <planeGeometry args={[100, 100]} />
-            <meshStandardMaterial color="#3f3f3f" />
-          </mesh>
-        </group>
-      </RigidBody>
-    </>
+    <RigidBody type="fixed" colliders={'cuboid'}>
+      <mesh receiveShadow position={[0, -1, 0]} rotation-x={-Math.PI / 2}>
+        <boxGeometry args={[100, 100, 2]} />
+        <meshStandardMaterial color="#3f3f3f" />
+      </mesh>
+    </RigidBody>
   );
 };
