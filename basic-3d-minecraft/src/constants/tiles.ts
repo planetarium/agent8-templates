124f25c5a12dd12d1c710c840cb6fc6f0f0cd7cc
Jam Park <minmpop@gmail.com>
2025-05-12 22:03:32 +0900

    remove: tile generator from voxel template

---
+++ basic-3d-minecraft/src/constants/tiles.ts
@@ -0,0 +1,37 @@
+// Extended tile types (25 types)
+export const TILE_TYPES = {
+  // Basic blocks - first row
+  GRASS: 1, // Green top grass block
+  DIRT: 2, // Brown dirt block
+  STONE: 3, // Gray stone block
+  COBBLESTONE: 4, // Textured stone block
+  BEDROCK: 5, // Dark gray bedrock
+
+  // More blocks - second row
+  WOOD_PLANKS: 6, // Wooden planks
+  SAPLING: 7, // Small tree sapling
+  BEDROCK_2: 8, // Alternative bedrock
+  WATER: 9, // Blue water
+  LAVA: 10, // Orange lava
+
+  // Third row
+  SAND: 11, // Yellow sand
+  GRAVEL: 12, // Gray gravel
+  GOLD_ORE: 13, // Gold ore in stone
+  IRON_ORE: 14, // Iron ore in stone
+  COAL_ORE: 15, // Coal ore in stone
+
+  // Fourth row
+  WOOD: 16, // Tree trunk
+  LEAVES: 17, // Tree leaves
+  GLASS: 18, // Transparent glass
+  WOOL_WHITE: 19, // White wool
+  FLOWER_YELLOW: 20, // Yellow flower
+
+  // Fifth row
+  FLOWER_RED: 21, // Red flower
+  MUSHROOM_BROWN: 22, // Brown mushroom
+  MUSHROOM_RED: 23, // Red mushroom
+  GOLD_BLOCK: 24, // Solid gold block
+  IRON_BLOCK: 25, // Solid iron block
+};
