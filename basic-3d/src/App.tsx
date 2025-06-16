77f3b49eb620f460f9ae8fce8897ae190e7b18d6
Jam Park <minmpop@gmail.com>
2025-06-16 17:47:29 +0900

    fix: update basic-3d view configuration

--- basic-3d/src/App.tsx
+++ basic-3d/src/App.tsx
@@ -1,19 +1,16 @@
-import { useState } from "react";
-import "./App.css";
+import { useState } from 'react';
+import './App.css';
 function App() {
   const [count, setCount] = useState(1);
 
   return (
-    <>
+    <div style={{ width: '100vw', height: '100vh' }}>
       <div className="card">
-        <button
-          onClick={() => setCount((count) => count + 1)}
-          className="bg-blue-500 text-white p-2 rounded-md"
-        >
+        <button onClick={() => setCount((count) => count + 1)} className="bg-blue-500 text-white p-2 rounded-md">
           count is {count}
         </button>
       </div>
-    </>
+    </div>
   );
 }
 
