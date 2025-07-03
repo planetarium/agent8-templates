6a77a27d91c1a87337ecf88b2cb3330b3dbe504f
rareboe <rareboy@naver.com>
2025-07-03 19:43:01 +0900

    feat: update use input controller in firstpersonview template.

--- basic-3d-firstpersonview/src/components/r3f/GameSceneCanvas.tsx
+++ basic-3d-firstpersonview/src/components/r3f/GameSceneCanvas.tsx
@@ -1,22 +1,13 @@
 import { Suspense } from 'react';
 import { Canvas } from '@react-three/fiber';
 import { Physics } from '@react-three/rapier';
-import { ControllerKeyMapping, FirstPersonViewController, FollowLight } from 'vibe-starter-3d';
+import { FirstPersonViewController, FollowLight } from 'vibe-starter-3d';
 import { useGameStore } from '../../stores/gameStore';
-import { Environment } from '@react-three/drei';
+import { Environment, Grid } from '@react-three/drei';
 import MapPhysicsReadyChecker from '../r3f/MapPhysicsReadyChecker';
 import EffectContainer from './EffectContainer';
 import Player from './Player';
 import Floor from './Floor';
-
-const movementKeyMapping: ControllerKeyMapping = {
-  forward: ['KeyW', 'ArrowUp'],
-  backward: ['KeyS', 'ArrowDown'],
-  leftward: ['KeyA', 'ArrowLeft'],
-  rightward: ['KeyD', 'ArrowRight'],
-  jump: ['Space'],
-  run: ['ShiftLeft', 'ShiftRight'],
-};
 
 /**
  * Game Scene Canvas Component
@@ -38,11 +29,25 @@ const GameSceneCanvas = () => {
           (e.target as HTMLCanvasElement).requestPointerLock();
         }}
       >
+        {/* For debugging purposes only */}
+        <Grid
+          args={[100, 100]}
+          position={[0, 0.1, 0]}
+          cellSize={1}
+          cellThickness={0.5}
+          cellColor="#6f6f6f"
+          sectionSize={5}
+          sectionThickness={1}
+          sectionColor="#9f9f9f"
+          fadeDistance={100}
+          fadeStrength={1}
+          userData={{ camExcludeCollision: true }}
+        />
         <Physics paused={!isMapPhysicsReady}>
           <Suspense fallback={null}>
             {/* ⚠️ MUST INCLUDE: Essential checker for map physics initialization */}
             {!isMapPhysicsReady && <MapPhysicsReadyChecker />}
-            <FirstPersonViewController keyMapping={movementKeyMapping} />
+            <FirstPersonViewController />
             <EffectContainer />
             <Environment preset="sunset" background={false} />
             <ambientLight intensity={0.7} />
