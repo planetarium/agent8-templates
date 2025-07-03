2ef1438b9d587407b30f01ee4bb66d3535e43bd5
rareboe <rareboy@naver.com>
2025-07-03 20:13:19 +0900

    chore: Remove use Grid component.

--- basic-3d-firstpersonview/src/components/r3f/GameSceneCanvas.tsx
+++ basic-3d-firstpersonview/src/components/r3f/GameSceneCanvas.tsx
@@ -3,7 +3,7 @@ import { Canvas } from '@react-three/fiber';
 import { Physics } from '@react-three/rapier';
 import { FirstPersonViewController, FollowLight } from 'vibe-starter-3d';
 import { useGameStore } from '../../stores/gameStore';
-import { Environment, Grid } from '@react-three/drei';
+import { Environment } from '@react-three/drei';
 import MapPhysicsReadyChecker from '../r3f/MapPhysicsReadyChecker';
 import EffectContainer from './EffectContainer';
 import Player from './Player';
@@ -29,20 +29,6 @@ const GameSceneCanvas = () => {
           (e.target as HTMLCanvasElement).requestPointerLock();
         }}
       >
-        {/* For debugging purposes only */}
-        <Grid
-          args={[100, 100]}
-          position={[0, 0.1, 0]}
-          cellSize={1}
-          cellThickness={0.5}
-          cellColor="#6f6f6f"
-          sectionSize={5}
-          sectionThickness={1}
-          sectionColor="#9f9f9f"
-          fadeDistance={100}
-          fadeStrength={1}
-          userData={{ camExcludeCollision: true }}
-        />
         <Physics paused={!isMapPhysicsReady}>
           <Suspense fallback={null}>
             {/* ⚠️ MUST INCLUDE: Essential checker for map physics initialization */}
